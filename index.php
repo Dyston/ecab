99f083c50a9e6079a1e225048cd96483f5a47350
andrew <andrew@Lenovo-G560>
2017-10-28 23:43:05 +0300

    end capture 1

--- index.php
+++ index.php
@@ -9,13 +9,27 @@
 
 $a=[1,4];
 $b= pi();
-unset($b);
-echo 'type of $a is ',gettype($a);
-if (isset($b)) {echo '<br>$b is create, $b=',$b;}else{echo'<br>$b is not create';};
-
+//unset($b);
+//echo 'type of $a is ',gettype($a);
+//if (isset($b)) {echo '<br>$b is create, $b=',$b;}else{echo'<br>$b is not create';};
+echo $b;
 echo '<br>';
 $link_on_b = &$b;
 echo '$link_on_b=',$link_on_b;
 
 //My english dictiionary
 if (!mysql_connect('krruda.mysql.tools','krruda_words','kxyt2xm7')) die('error');
+echo '<br>connection to DB is good!';
+echo '<br>You OS is <b>', PHP_OS,'</b>';
+echo '<br>You PHP ver. is <b>',PHP_VERSION,'</b>';
+echo '<br>';
+$predir='You are <b>';
+$dir=`whoami`;
+echo $predir.$dir.'</b>';
+echo '<br>';
+for ($i=0;$i< iconv_strlen($dir);$i++) 
+{
+    if ($i%2===0) {
+echo $dir[$i],'<br>';
+    }
+}
