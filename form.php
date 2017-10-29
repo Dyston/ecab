87982b5733919b9880b738bfad8e58226cae0788
andrew <andrew@Lenovo-G560>
2017-10-29 20:35:44 +0200

    Ended work at home. Start Chapter 7.

---
+++ form.php
@@ -0,0 +1,21 @@
+<form action="index.php" method="GET">
+    Enter you name: <input type="text" name="name">
+    <input type="submit" value="Ok" name="send">
+</form>
+
+
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author andrew
+ */
+interface form {
+    //put your code here
+}
